5e2493490726c81b0031d770e0455e17bc661944
Zishan Liu <zisaanlau@gmail.com>
2020-08-04 17:19:47 -0400

    visa_status_update finished

--- src/main/java/com/bf/employee/EmployeeApplication.java
+++ src/main/java/com/bf/employee/EmployeeApplication.java
@@ -2,9 +2,11 @@ package com.bf.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ImportResource;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
+//@SpringBootApplication
 @ImportResource("beans.xml")
 public class EmployeeApplication {
 
--- src/main/java/com/bf/employee/service/VisaStatusService.java
+++ src/main/java/com/bf/employee/service/VisaStatusService.java
@@ -4,4 +4,7 @@ import com.bf.employee.entity.VisaStatus;
 
 public interface VisaStatusService {
     public VisaStatus getVisaStatus(int employeeVisaStatusId);
+
+    public Boolean updateVisaType(int employeeVisaStatusId, String visaType);
+
 }
